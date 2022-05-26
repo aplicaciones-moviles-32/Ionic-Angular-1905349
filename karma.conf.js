0b9149f126019ee4ec04a4da4f13e4720b40c1e7
Josue Reyes Cabral <70187068+Pablo-design25@users.noreply.github.com>
2022-05-26 12:40:12 -0500

    Add files via upload

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/ionic-angular-11905349'),
+      dir: require('path').join(__dirname, './coverage/IA-11905349'),
       subdir: '.',
       reporters: [
         { type: 'html' },
