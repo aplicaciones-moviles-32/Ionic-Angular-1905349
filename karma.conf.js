0efd16a79b6f26767ddb8fabfbe8ba50527c529e
Josue Reyes Cabral <70187068+Pablo-design25@users.noreply.github.com>
2022-05-26 11:08:31 -0500

    Add files via upload

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/IA-1905340'),
+      dir: require('path').join(__dirname, './coverage/IA-1905349'),
       subdir: '.',
       reporters: [
         { type: 'html' },
